1b3c3877f84a8904469d368ee693dd34aba93205
ge85riz <ge85riz@tum.de>
2025-07-23 22:47:44 +0200

    🐛 fix: move notes field from EmployeeDetailsDTO to ProfileDetailsDTO

--- backend/src/main/java/com/itestra/eep/dtos/EmployeeDetailsDTO.java
+++ backend/src/main/java/com/itestra/eep/dtos/EmployeeDetailsDTO.java
@@ -20,8 +20,6 @@ public class EmployeeDetailsDTO implements Serializable {
 
     LocalDate employmentStartDate;
 
-    String notes;
-
     String location;
 
     List<EmployeeParticipationDetailsDTO> participations;
